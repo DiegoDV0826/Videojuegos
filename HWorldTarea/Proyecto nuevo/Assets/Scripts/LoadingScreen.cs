752ee5731d7a444cda33051a1d01d57132b448eb
Diego <A01747310@itesm.mx>
2020-04-02 19:44:11 -0600

    asd

---
+++ HWorldTarea/Proyecto nuevo/Assets/Scripts/LoadingScreen.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadingScreen : MonoBehaviour
+{
+    public GameObject loadingScreenObj;
+    public Slider Slider;
+
+    AsyncOperation async;
+
+    public void LoadScreen()
+    {
+        StartCoroutine(LoadingScreenEnum());
+    }
+    IEnumerator LoadingScreenEnum() 
+    {
+        loadingScreenObj.SetActive(true);
+        async = SceneManager.LoadSceneAsync(0);
+        async.allowSceneActivation = false;
+        while(async.isDone == false)
+        {
+            Slider.value = async.progress;
+            if(async.progress == 0.9f)
+            {
+                Slider.value = 1f;
+                async.allowSceneActivation = true;
+            }
+            yield return null;
+        }
+    }
+}
